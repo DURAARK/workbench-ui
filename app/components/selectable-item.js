2c86a06bf43250877b747a51236096e40d08ef94
Martin Hecher <martin.hecher@fraunhofer.at>
2015-02-13 15:16:04 +0100

    code cleanup

--- app/components/selectable-item.js
+++ app/components/selectable-item.js
@@ -4,15 +4,7 @@ export
 default Ember.Component.extend({
     actions: {
         selectItem: function(item) {
-            var item = this.get('item');
             this.sendAction('action', item);
         }
     }
-
-    // change: function() {
-    //     var item = this.get('item');
-    //     this.sendAction('action', item);
-
-    //     console.log('itemcomponent selected: ' + item.get('path'));
-    // }
 });
