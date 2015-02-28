9da25480f4024c111e8d2bd14332dc142fa272d6
Martin Hecher <martin.hecher@fraunhofer.at>
2015-02-28 16:17:33 +0100

    towards a configurable search dialog...

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -16,6 +16,7 @@ export default Ember.Controller.extend({
 			});
 		});
 		this.set('markers', markers);
+		console.log('updated markers');
 	}.observes('buildings'),
 
 	onErrorMessageChanged: function() {
@@ -51,9 +52,54 @@ export default Ember.Controller.extend({
 			console.log('Added constraint | ' + selectedProperty + ': ' + propertyValue);
 		},
 
+		removeSearchConstraint: function(constraint) {
+			var constraints = this.get('searchConstraints');
+			constraints.removeObject(constraint);
+		},
+
 		selectProperty: function(property) {
 			console.log('selectedProperty: ' + property);
 			this.set('selectedProperty', property);
+		},
+
+		listAll: function() {
+			debugger;
+			this.store.find('physicalAsset').then(function(records) {
+				debugger;
+				console.log('records: ' + records.get('length'));
+				this.set('buildings', records);
+			}.bind(this));
+		},
+
+		performSearch: function() {
+			var constraints = this.get('searchConstraints'),
+				that = this;
+
+			if (!constraints.length) {
+				this.set('errorMessage', 'You have to add a search constraint first!');
+				return;
+			}
+
+			var data = JSON.parse(JSON.stringify(constraints, null, 4));
+			console.log('Searching for: ' + JSON.stringify(data, null, 4));
+
+			var payload = {
+				constraints: data
+			};
+
+			$.ajax({
+				type: 'POST',
+				url: 'http://localhost:5005/example/search',
+				data: payload
+			}).done(function(response) {
+				that.store.unloadAll('physicalAsset');
+				that.store.pushMany('physicalAsset', response);
+				// console.log('Search response: ' + JSON.stringify(response, 4, null));
+
+				that.set('buildings', that.store.all('physicalAsset'));
+			}).fail(function(error) {
+				console.log('Search error: ' + JSON.stringify(error, 4, null));
+			})
 		}
 	}
 });
