6490fb775a155ba396684fa683ac85cab348bd5f
Martin Hecher <martin.hecher@fraunhofer.at>
2015-02-28 13:59:43 +0100

    search constraints can be created now

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	selectedProperty: null,
+	errorMessage: null,
+	searchConstraints: Ember.A(),
 
 	onBuildingsChanged: function() {
 		var markers = [],
@@ -14,5 +16,44 @@ export default Ember.Controller.extend({
 			});
 		});
 		this.set('markers', markers);
-	}.observes('buildings')
+	}.observes('buildings'),
+
+	onErrorMessageChanged: function() {
+		var message = this.get('errorMessage');
+
+		if (message) {
+			// TODO: replace with a popup message!
+			alert('Error: ' + message);
+			this.set('errorMessage', null);
+		}
+	}.observes('errorMessage'),
+
+	actions: {
+		addSearchConstraint: function() {
+			var selectedProperty = this.get('selectedProperty'),
+				propertyValue = this.get('propertyValue');
+
+			if (!selectedProperty) {
+				this.set('errorMessage', 'Please select a property first!');
+				return;
+			}
+
+			if (!propertyValue) {
+				this.set('errorMessage', 'Please enter a value for the property first!');
+				return;
+			}
+
+			var constraints = this.get('searchConstraints');
+			constraints.pushObject({
+				property: selectedProperty,
+				value: propertyValue
+			});
+			console.log('Added constraint | ' + selectedProperty + ': ' + propertyValue);
+		},
+
+		selectProperty: function(property) {
+			console.log('selectedProperty: ' + property);
+			this.set('selectedProperty', property);
+		}
+	}
 });
