762415a07387b6e04777072b08409d7a14b88b29
Martin Hecher <martin.hecher@fraunhofer.at>
2015-03-08 12:01:14 +0100

    enhanced file selection templates

--- app/components/file-item.js
+++ app/components/file-item.js
@@ -2,9 +2,31 @@ import Ember from 'ember';
 
 export
 default Ember.Component.extend({
-    actions: {
-        onDelete: function(item) {
-            this.sendAction('onDelete', item);
-        }
-    }
+	actions: {
+		onDelete: function(item) {
+			this.sendAction('onDelete', item);
+		}
+	},
+
+	filename: function() {
+		var path = this.get('item.path');
+		if (path) {
+			return path.split('/').pop();
+		}
+
+		return '';
+	}.property('item'),
+
+	extension: function() {
+		var path = this.get('item.path');
+		if (path) {
+			return _getFileExtension(path)[0];
+		}
+		
+		return '';
+	}.property('item')
 });
+
+function _getFileExtension(filepath) {
+	return (/[.]/.exec(filepath)) ? /[^.]+$/.exec(filepath) : null;
+}
--- app/components/selectable-item.js
+++ app/components/selectable-item.js
@@ -2,9 +2,31 @@ import Ember from 'ember';
 
 export
 default Ember.Component.extend({
-    actions: {
-        selectItem: function(item) {
-            this.sendAction('action', item);
-        }
-    }
+	actions: {
+		selectItem: function(item) {
+			this.sendAction('action', item);
+		}
+	},
+
+	filename: function() {
+		var path = this.get('item.path');
+		if (path) {
+			return path.split('/').pop();
+		}
+
+		return '';
+	}.property('item'),
+
+	extension: function() {
+		var path = this.get('item.path');
+		if (path) {
+			return _getFileExtension(path)[0];
+		}
+
+		return '';
+	}.property('item')
 });
+
+function _getFileExtension(filepath) {
+	return (/[.]/.exec(filepath)) ? /[^.]+$/.exec(filepath) : null;
+}
