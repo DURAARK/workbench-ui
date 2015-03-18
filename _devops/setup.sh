17bc6547a24f6d1d45de00540dc0841745fc5ec5
Martin Hecher <martin.hecher@fraunhofer.at>
2015-03-18 17:50:19 +0100

    added fix for 'hasMagic' dependency problem (rimraf, glob, ember-cli)

--- _devops/setup.sh
+++ _devops/setup.sh
@@ -6,4 +6,5 @@ SRC=$(sed '5q;d' $SERVICEFILE)
 
 echo "Setting up project:"
 echo "  * installing local dependencies: (cd $SRC && npm install && bower install)"
+sudo npm install ember-cli -g
 (cd $SRC && npm install && bower install --allow-root)
