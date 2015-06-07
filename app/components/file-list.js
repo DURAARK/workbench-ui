277ac8051c6cdd0c011856da0e40bab709940f88
Martin Hecher <martin.hecher@fraunhofer.at>
2015-06-07 15:26:49 +0200

    fix(gui): fixed file details page

--- app/components/file-list.js
+++ app/components/file-list.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	actions: {
+		removeFile: function() {
+			console.log('removed file: ');
+		},
+
+		detailsFile: function() {
+			console.log('show file details for: ');
+			this.get('currentController').transitionToRoute('fileDetails');
+		}
+	}
 });
