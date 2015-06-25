7d00963ce693a39c7bdf722d33a3951fb169f350
martinhecher <martin.hecher@fraunhofer.at>
2015-06-25 10:59:46 +0200

    added JSON-LD encoded buildm to session fixture for testing

--- app/routes/sip.js
+++ app/routes/sip.js
@@ -11,69 +11,213 @@ default Ember.Route.extend({
             sessions: [{
                 label: 'Haus 30',
 
+                buildm: [{
+                    "@id": "http://data.duraark.eu/resource/ifcspffile_d86c761c42e440659a8a5b945f695b76",
+                    "@type": [
+                        "http://data.duraark.eu/vocab/IFCSPFFile"
+                    ],
+                    "http://data.duraark.eu/vocab/actorCount": [{
+                        "@value": "0",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/authoringTool": [{
+                        "@value": "20140606_1530(x64) - Exporter 15.2.0.0 - Alternate UI 15.2.0.0"
+                    }, {
+                        "@value": "Autodesk Revit 2015 (ENU)"
+                    }, {
+                        "@value": "The EXPRESS Data Manager Version 5.02.0100.07 : 28 Aug 2013"
+                    }],
+                    "http://data.duraark.eu/vocab/creator": [{
+                        "@value": ""
+                    }, {
+                        "@value": "Nancy "
+                    }],
+                    "http://data.duraark.eu/vocab/dateCreated": [{
+                        "@value": "2015-01-12T12:04:17"
+                    }],
+                    "http://data.duraark.eu/vocab/dimensionCount": [{
+                        "@value": "3",
+                        "@type": "http://www.w3.org/2001/XMLSchema#integer"
+                    }],
+                    "http://data.duraark.eu/vocab/entityCount": [{
+                        "@value": "86",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/fileSchema": [{
+                        "@value": "IFC2X3"
+                    }],
+                    "http://data.duraark.eu/vocab/geometricPrecision": [{
+                        "@value": "0.01",
+                        "@type": "http://www.w3.org/2001/XMLSchema#decimal"
+                    }],
+                    "http://data.duraark.eu/vocab/hasType": [{
+                        "@value": "Model"
+                    }],
+                    "http://data.duraark.eu/vocab/instanceCount": [{
+                        "@value": "5311",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/name": [{
+                        "@value": "001"
+                    }],
+                    "http://data.duraark.eu/vocab/optionalAttributesSet": [{
+                        "@value": "0.520128179451",
+                        "@type": "http://www.w3.org/2001/XMLSchema#double"
+                    }],
+                    "http://data.duraark.eu/vocab/relationshipCount": [{
+                        "@value": "362",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/unit": [{
+                        "@value": "KILONEWTON"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Ampere"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Candela"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#CubicMeter"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#DegreeCelsius"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Hertz"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Kilogram"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Lumen"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Lux"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Millimeter"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Pascal"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#SecondTime"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#SquareMeter"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Volt"
+                    }, {
+                        "@id": "http://qudt.org/vocab/unit#Watt"
+                    }]
+                }, {
+                    "@id": "http://data.duraark.eu/resource/physicalasset_d86c761c42e440659a8a5b945f695b76",
+                    "@type": [
+                        "http://data.duraark.eu/vocab/PhysicalAsset"
+                    ],
+                    "http://data.duraark.eu/vocab/buildingCount": [{
+                        "@value": "1",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/columnCount": [{
+                        "@value": "0",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/componentCount": [{
+                        "@value": "129",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/doorCount": [{
+                        "@value": "17",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/floorCount": [{
+                        "@value": "2",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/identifier": [{
+                        "@value": "3OR7OSGkH0PPgAMvHVQLjs"
+                    }],
+                    "http://data.duraark.eu/vocab/latitude": [{
+                        "@value": "42.3582999997",
+                        "@type": "http://www.w3.org/TR/xmlschema11-2/#double"
+                    }],
+                    "http://data.duraark.eu/vocab/longitude": [{
+                        "@value": "-71.0602999997",
+                        "@type": "http://www.w3.org/TR/xmlschema11-2/#double"
+                    }],
+                    "http://data.duraark.eu/vocab/name": [{
+                        "@value": "001",
+                        "@type": "http://schema.org/Text"
+                    }],
+                    "http://data.duraark.eu/vocab/spaceCount": [{
+                        "@value": "0",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/streetAddress": [{
+                        "@value": "Enter address here"
+                    }],
+                    "http://data.duraark.eu/vocab/wallCount": [{
+                        "@value": "46",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }],
+                    "http://data.duraark.eu/vocab/windowCount": [{
+                        "@value": "14",
+                        "@type": "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                    }]
+                }],
                 physicalAssets: [{
                     descMD: {
-                            Identifier: 'f81d4fae-7dec-11d0-a765-00a0c91e6bf6', // mandatory, exactly one allowed, string
-                            name: 'Haus 30', // mandatory, multiple allowed, string
-                            latitude: '14', // mandatory, exactly one, string
-                            longitude: '43', // mandatory, exactly one, string
-                            owner: 'Haus 30 owner', // optional, multiple allowed, string
-                            buildingArea: '2943', // optional, exactly one allowed, string                                
-                            floorCount: 4, // optional, exactly one allowed, integer                               
-                            numberOfRooms: 23, // optional, exactly one allowed, integer                               
-                            'function': 'Acts as Haus 30', // optional, multiple allowed, string
-                            architecturalStyle: 'Haus 30 style', // optional, multiple allowed, string
-                            description: 'The Haus 30', // optional, multiple allowed, string
-                            location: 'Haus 30 Strasse 42', // optional, exactly one allowed, string
-                            streetAddress: 'Haus 30 Strasse 42' // optional, exactly one allowed, string
+                        Identifier: 'f81d4fae-7dec-11d0-a765-00a0c91e6bf6', // mandatory, exactly one allowed, string
+                        name: 'Haus 30', // mandatory, multiple allowed, string
+                        latitude: '14', // mandatory, exactly one, string
+                        longitude: '43', // mandatory, exactly one, string
+                        owner: 'Haus 30 owner', // optional, multiple allowed, string
+                        buildingArea: '2943', // optional, exactly one allowed, string                                
+                        floorCount: 4, // optional, exactly one allowed, integer                               
+                        numberOfRooms: 23, // optional, exactly one allowed, integer                               
+                        'function': 'Acts as Haus 30', // optional, multiple allowed, string
+                        architecturalStyle: 'Haus 30 style', // optional, multiple allowed, string
+                        description: 'The Haus 30', // optional, multiple allowed, string
+                        location: 'Haus 30 Strasse 42', // optional, exactly one allowed, string
+                        streetAddress: 'Haus 30 Strasse 42' // optional, exactly one allowed, string
 
-                            // TODO: convert remaining properties from XML below:
+                        // TODO: convert remaining properties from XML below:
 
-                            // <xs:element name="postalCodeStart" type="xs:string" minOccurs="0">
+                        // <xs:element name="postalCodeStart" type="xs:string" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="postalCodeEnd" type="xs:string" minOccurs="0">
+                        // </xs:element>
+                        // <xs:element name="postalCodeEnd" type="xs:string" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="postOfficeBoxNumber" type="xs:string" minOccurs="0">
+                        // </xs:element>
+                        // <xs:element name="postOfficeBoxNumber" type="xs:string" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="addressRegion" type="xs:string" minOccurs="0">
+                        // </xs:element>
+                        // <xs:element name="addressRegion" type="xs:string" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="addressCountry" type="xs:string" minOccurs="0">
+                        // </xs:element>
+                        // <xs:element name="addressCountry" type="xs:string" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="postalLocality" type="xs:string" minOccurs="0">
+                        // </xs:element>
+                        // <xs:element name="postalLocality" type="xs:string" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="architect" type="xs:string" minOccurs="0" maxOccurs="unbounded">
+                        // </xs:element>
+                        // <xs:element name="architect" type="xs:string" minOccurs="0" maxOccurs="unbounded">
 
-                            // </xs:element>
-                            // <xs:element name="contributor" type="xs:string" minOccurs="0" maxOccurs="unbounded">
+                        // </xs:element>
+                        // <xs:element name="contributor" type="xs:string" minOccurs="0" maxOccurs="unbounded">
 
-                            // </xs:element>
-                            // <xs:element name="startDate" type="xs:integer" minOccurs="0">
+                        // </xs:element>
+                        // <xs:element name="startDate" type="xs:integer" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="completionDate" type="xs:integer" minOccurs="0">
+                        // </xs:element>
+                        // <xs:element name="completionDate" type="xs:integer" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="constructionTime" type="xs:integer" minOccurs="0">
+                        // </xs:element>
+                        // <xs:element name="constructionTime" type="xs:integer" minOccurs="0">
 
-                            // </xs:element>
-                            // <xs:element name="rebuildingDate" type="xs:date" minOccurs="0" maxOccurs="unbounded">
+                        // </xs:element>
+                        // <xs:element name="rebuildingDate" type="xs:date" minOccurs="0" maxOccurs="unbounded">
 
-                            // </xs:element>
-                            // <xs:element name="modificationDetails" type="xs:string" minOccurs="0" maxOccurs="unbounded">
+                        // </xs:element>
+                        // <xs:element name="modificationDetails" type="xs:string" minOccurs="0" maxOccurs="unbounded">
 
-                            // </xs:element>
-                            // <xs:element name="cost" type="xs:double" minOccurs="0" maxOccurs="unbounded">
+                        // </xs:element>
+                        // <xs:element name="cost" type="xs:double" minOccurs="0" maxOccurs="unbounded">
 
-                            // </xs:element>
-                            // <xs:element name="rightsDetails" type="xs:string" minOccurs="0" maxOccurs="unbounded">
+                        // </xs:element>
+                        // <xs:element name="rightsDetails" type="xs:string" minOccurs="0" maxOccurs="unbounded">
 
-                            // </xs:element>
+                        // </xs:element>
                     },
                     digitalObjects: [{
                         label: 'Haus 30',
