942481f4200190b102c93b79a2c151ecfff1d01c
Martin Hecher <martin.hecher@fraunhofer.at>
2015-07-02 23:42:25 +0200

    working on 'files' design

--- app/components/buildm-viewer.js
+++ app/components/buildm-viewer.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function() {
+    console.log('bin da');
+    var controller = this;
+    var dropdown = document.querySelectorAll('.dropdown-list'),
+      dropdownArray = Array.prototype.slice.call(dropdown, 0);
+
+    dropdownArray.forEach(function(el) {
+      var button = el.querySelector('a[dropdown-prop="title"]'),
+        menu = el.querySelector('.dropdown-list-items'),
+        icon = button.querySelector('i.dropdown-icon'),
+
+        toggleDropdown = function() {
+          $(menu).toggleClass('dropdown-list-open');
+          $(icon).toggleClass('dropdown-icon-open');
+        };
+
+      button.addEventListener('click', toggleDropdown);
+    });
+  }
 });
--- app/controllers/files.js
+++ app/controllers/files.js
@@ -17,8 +17,21 @@ export default Ember.Controller.extend({
 
       controller.set('isLoadingMetadata', true);
 
+      // TODO: check local store before sending network request!
+      // this.store.all('metadatum', {
+      //   path: file.get('path'),
+      //   type: file.get('type')
+      // }).then(function(md) {
+      //   debugger;
+      //   if (md.length) {
+      //     controller.set('fileInfo', md[0]);
+      //     controller.set('isLoadingMetadata', false);
+      //   }
+      // });
+
       // NOTE: requests metadata for the given file via the
       //       'metadata-extraction' service
+      // FIXXME: utilize store as cache, do not request every time!
       var md = controller.store.createRecord('metadatum');
       md.set('path', file.get('path'));
       md.set('type', file.get('type'));
@@ -30,6 +43,5 @@ export default Ember.Controller.extend({
         controller.set('isLoadingMetadata', false);
       });
     }
-
   }
 });
