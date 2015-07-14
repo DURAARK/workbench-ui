9cd56befd1f13ab75f977420577d246ef083fcb6
martinhecher <martin.hecher@fraunhofer.at>
2015-07-14 17:38:23 +0200

    added geometric tools and made them togglable

--- app/components/geo-tool.js
+++ app/components/geo-tool.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+export
+default Ember.Component.extend({
+  tagName: '',
+  isChecked: false,
+
+  onIsCheckedChange: function() {
+    var digObj = this.get('digitalObject'),
+      toolName = this.get('label'),
+      isChecked = this.get('isChecked');
+
+    this.sendAction('toggleSelection', digObj, toolName, isChecked);
+  }.observes('isChecked')
 });
--- app/controllers/geometricenrichment.js
+++ app/controllers/geometricenrichment.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    next: function() {
+
+      // FIXXME: check if everytihng is saved in the buildm-editor and display modal in case of unsaved changes!
+
+      var session = this.get('session');
+      this.transitionToRoute('sipgeneration', session);
+    },
+
+    back: function() {
+
+      // FIXXME: check if everytihng is saved in the buildm-editor and display modal in case of unsaved changes!
+
+      var session = this.get('session');
+      this.transitionToRoute('semanticenrichment', session);
+    },
+
+    toggleToolSelection: function(digObj, toolName, isChecked) {
+      if (isChecked) {
+        console.log('Added "' + toolName + '" on digital object: ' + digObj.label);
+      } else {
+        console.log('Removed tool "' + toolName + '" on digital object: ' + digObj.label);
+      }
+    }
+  }
 });
