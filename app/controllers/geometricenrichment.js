9d7501bdee6aef4b9f0d576ec9dd5bdbdd41eea1
martinhecher <martin.hecher@fraunhofer.at>
2015-07-15 12:21:18 +0200

    changed 'geometricenrichment' route to work like the 'semanticenrichment' route

--- app/controllers/geometricenrichment.js
+++ app/controllers/geometricenrichment.js
@@ -24,6 +24,36 @@ export default Ember.Controller.extend({
       } else {
         console.log('Removed tool "' + toolName + '" on digital object: ' + digObj.label);
       }
+    },
+
+    showTopicSelection: function(digObj) {
+      this.set('fileInfo', digObj);
+    },
+
+    clickedTool: function(topic) {
+      var selectedDigitalObject = this.get('fileInfo'),
+        currentTools = selectedDigitalObject.get('geoTools');
+
+      var isTopic = currentTools.find(function(item) {
+        return topic.get('label') === item.label;
+      });
+
+      if (isTopic) {
+        currentTools.removeObject(topic);
+      } else {
+        currentTools.pushObject(topic);
+      }
+    },
+
+    // FIXXME: change name!
+    removeTopic: function(digObj, topic) {
+      digObj.get('geoTools').removeObject(topic);
+    },
+
+    // FIXXME: change name!
+    showSelectedTopic: function(digObj, tool) {
+      this.set('fileInfo', null);
+      this.set('tool', tool);
     }
   }
 });
--- app/routes/geometricenrichment.js
+++ app/routes/geometricenrichment.js
@@ -11,6 +11,41 @@ export default Ember.Route.extend({
   setupController: function(controller, model) {
     this._super(controller, model);
 
+    var session = model,
+      digObjs = [];
+
     controller.set('session', model);
+
+    session.get('digitalObjects').forEach(function(digObj) {
+      var obj = Ember.Object.create({
+        label: digObj.label,
+        buildm: digObj.buildm,
+        semMD: Ember.Object.create(digObj.semMD),
+        techMD: digObj.techMD,
+        derivatives: digObj.derivatives,
+        geoTools: []
+      });
+
+      digObjs.pushObject(obj);
+    });
+
+    controller.set('digitalObjects', digObjs);
+
+    // FIXXME: get from SDA service!
+    // FIXXME: create Topic model to enable saving and linking into session model!
+    var tools = [Ember.Object.create({
+      label: 'IFC Reconstruction',
+      description: 'If enabled an IFC file is reconstructed from the point cloud scan and added to the archive.'
+    }), Ember.Object.create({
+      label: 'Electrical Applicane Detection',
+      description: 'If enabled an IFC file is generated containing a hypothesis of the in-wall electrical applicances of the building, which are extracted from the point cloud scan.'
+    })];
+
+    // FIXXME: incorporate selected tools from session!
+    tools.forEach(function(tool) {
+      tool.set('isSelected', false);
+    });
+
+    controller.set('tools', tools);
   }
 });
