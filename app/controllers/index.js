7a096739f2fe8fe078cc8948dd2456f0c19bff17
martinhecher <martin.hecher@fraunhofer.at>
2015-07-15 11:53:51 +0200

    fixed entry link on landing page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,12 +4,7 @@ export default Ember.Controller.extend({
   actions: {
     openArchivalSession: function() {
       console.log('[info] Open archival session ...');
-      this.transitionToRoute('workflows.files');
-    },
-
-    openRetrievalSession: function() {
-      console.log('[info] Open retrieval session ...');
-      this.transitionToRoute('workflows.retrieval');
+      this.transitionToRoute('files', 1);
     }
   }
 });
