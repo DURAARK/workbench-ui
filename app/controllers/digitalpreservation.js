bb52c1dae1a06c060f984e0f281ec642287c18e4
martinhecher <martin.hecher@fraunhofer.at>
2015-08-04 17:58:23 +0200

    * added 'bagIt' download functionality * added 'Rosetta' deposit functionality (disabled for the public version)

--- app/controllers/digitalpreservation.js
+++ app/controllers/digitalpreservation.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import ENV from '../config/environment';
 
-var sipGeneratorEndpoint = ENV.DURAARKAPI.sipgenerator;
+var sipGeneratorEndpoint = ENV.DURAARKAPI.digitalpreservation;
 
 function post(url, data) {
   var that = this;
@@ -29,37 +29,83 @@ function post(url, data) {
 export default Ember.Controller.extend({
   actions: {
     createRosettaSIP: function() {
-      console.log('Schedule Rosetta SIP creation ...');
-
-      var url = sipGeneratorEndpoint.host + '/sip',
-        session = this.get('session');
-
-      var payload = {
-        session: session.toJSON(),
-        output: 'rosetta'
-      };
-
-      payload.session.digitalObjects = [];
-
-      session.get('digitalObjects').forEach(function(dobj) {
-        payload.session.digitalObjects.push(JSON.parse(JSON.stringify(dobj)));
-      });
-
-      console.log('url: ' + url);
-
-      // FIXXME: I also found no way to update the session with ember board utilities, I guess I have an
-      // logical error in my approach, it cannot be that hard with ember data and plain objects. Anyways,
-      // this does the job, too:
-      post(url, payload).then(function(result) {
-        console.log('created SIP at: ' + JSON.stringify(result, null, 4));
-      }).catch(function(err) {
-        throw new Error(err);
-      });
+      // FIXXME: add authentification mechanism!
+      alert('Data deposit to Rosetta is disabled for the public version, to not overload the system. You can use the "Download BagIt SIP" option.');
+      
+      // console.log('Schedule BagIt SIP creation ...');
+      //
+      // let session = this.get('session'),
+      //   url = sipGeneratorEndpoint.host + '/sip',
+      //   controller = this;
+      //
+      // var plainSession = {};
+      //
+      // plainSession['physicalAssets'] = session.get('physicalAssets').toArray();
+      // plainSession['digitalObjects'] = session.get('digitalObjects').toArray();
+      //
+      // let body = {
+      //   session: [plainSession],
+      //   output: {
+      //     type: 'rosetta'
+      //   }
+      // };
+      //
+      // controller.set('rosettaIsCreating', true);
+      //
+      // _post(url, body).then(function(result) {
+      //   console.log('Sucessfully created Rosetta SIP at: ' + JSON.stringify(result, 4, null));
+      //   controller.set('rosettaIsCreating', false);
+      // }).catch(function(err) {
+      //   controller.set('rosettaIsCreating', false);
+      //   throw new Error(err);
+      // });
     },
 
     createBagItSIP: function() {
       console.log('Schedule BagIt SIP creation ...');
 
+      let session = this.get('session'),
+        url = sipGeneratorEndpoint.host + '/sip',
+        controller = this;
+
+      var plainSession = {};
+
+      plainSession['physicalAssets'] = session.get('physicalAssets').toArray();
+      plainSession['digitalObjects'] = session.get('digitalObjects').toArray();
+
+      let body = {
+        session: [plainSession],
+        output: {
+          type: 'bag'
+        }
+      };
+
+      controller.set('bagIsCreating', true);
+
+      _post(url, body).then(function(result) {
+        console.log('Sucessfully created BagIt SIP at: ' + JSON.stringify(result, 4, null));
+        console.log('Download-URL: ' + sipGeneratorEndpoint.host + result.url);
+
+        function downloadURL(url) {
+          var hiddenIFrameID = 'hiddenDownloader',
+            iframe = document.getElementById(hiddenIFrameID);
+          if (iframe === null) {
+            iframe = document.createElement('iframe');
+            iframe.id = hiddenIFrameID;
+            iframe.style.display = 'none';
+            document.body.appendChild(iframe);
+          }
+          iframe.src = url;
+
+          controller.set('bagIsCreating', false);
+        };
+
+        console.log('Downloading from: ' + sipGeneratorEndpoint.host + result.url);
+        downloadURL(sipGeneratorEndpoint.host + result.url);
+      }).catch(function(err) {
+        controller.set('bagIsCreating', false);
+        throw new Error(err);
+      });
     },
 
     back: function() {
@@ -71,3 +117,27 @@ export default Ember.Controller.extend({
     },
   }
 });
+
+var _post = function(url, data) {
+  var that = this;
+
+  return new Ember.RSVP.Promise(function(resolve, reject) {
+    function handler(data, status, jqxhr) {
+      if (status === 'success') {
+        resolve(data);
+      } else {
+        reject(new Error('[_post]: "' + url + '" failed with status: [' + jqxhr.status + ']'));
+      }
+    }
+
+    // Ember.$.post(url, data, handler);
+    Ember.$.ajax({
+      type: 'POST',
+      url: url,
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      data: JSON.stringify(data),
+      success: handler
+    });
+  });
+}
