626822ed658892bc2fc21010a2d08e99c32e8872
martinhecher <martin.hecher@fraunhofer.at>
2015-08-05 16:54:13 +0200

    open 'electrical-appliance' result image in new tab on click

--- app/components/electrical-appliance.js
+++ app/components/electrical-appliance.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    clickImage: function(img) {
+      var win = window.open(img.src, '_blank');
+      win.focus();
+    }
+  }
 });
