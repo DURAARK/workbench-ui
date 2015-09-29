da978828a3bd6ac88eba4a7ef883876294408f99
martinhecher <martin.hecher@fraunhofer.at>
2015-09-29 14:58:46 +0200

    added 'duraark-header'

---
+++ app/components/duraark-header.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // didInsertElement: function() {
+  //   var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
+  //   $('#js-centered-navigation-menu').removeClass("show");
+  //
+  //   menuToggle.on('click', function(e) {
+  //     e.preventDefault();
+  //     $('#js-centered-navigation-menu').slideToggle(function() {
+  //       if ($('#js-centered-navigation-menu').is(':hidden')) {
+  //         $('#js-centered-navigation-menu').removeAttr('style');
+  //       }
+  //     });
+  //   });
+  // }
+});
