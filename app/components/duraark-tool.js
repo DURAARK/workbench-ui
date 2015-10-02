5513718c9c4c243a69ae63387d82fedd61aa2031
martinhecher <martin.hecher@fraunhofer.at>
2015-10-02 16:04:59 +0200

    introduced 'duraark-tool' component as visual wrappers for ifc-reconstruction, rise and semanticenrichment tools

---
+++ app/components/duraark-tool.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isFullscreen: false,
+
+  actions: {
+    close() {
+      this.sendAction('close');
+    },
+
+    fullscreen() {
+      this.toggleProperty('isFullscreen', true);
+    }
+  }
+});
--- app/components/ifc-reconstruction.js
+++ app/components/ifc-reconstruction.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isFullscreen: false,
-  
-  actions: {
-    close() {
-      this.sendAction('close');
-    },
-
-    fullscreen() {
-      this.toggleProperty('isFullscreen', true);
-    }
-  }
 });
