fa6e674e3d5a5344492db1694eea60b21e6cc166
martinhecher <martin.hecher@fraunhofer.at>
2015-10-12 20:57:04 +0200

    get 'upload' url from duraark service

--- app/components/x-droplet.js
+++ app/components/x-droplet.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend(Droplet, {
-  // url: 'http://juliet.cgv.tugraz.at/api/v0.7/sessions/files/upload',
+  url: function() {
+    return this.duraark.getAPIEndpoint('sessions') + '/uploads/upload';
+  },
   // url: 'http://localhost/api/v0.7/sessions/files/upload',
-  url: 'http://localhost:5011/uploads/upload',
+  // url: 'http://localhost:5011/uploads/upload',
   // see http://craftcms.stackexchange.com/questions/2328/413-request-entity-too-large-error-with-uploading-a-file
 
   hooks: {
