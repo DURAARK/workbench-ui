d4eb1e993a65ebdabee7f0e331064a312ced3b0e
Martin Hecher <martin.hecher@fraunhofer.at>
2015-10-26 12:56:06 +0100

    added 'duraark-sidebar' component for fullscreen scroll bar logic

---
+++ app/components/duraark-sidebar.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // FIXXME: get correct values for those!
+  headerHeight: 64,
+  workflowHeight: 40,
+  footerHeight: 52,
+  offset: 18,
+
+  setFullHeight() {
+    let $el = this.$(),
+      headerHeight = this.get('headerHeight'),
+      footerHeight = this.get('footerHeight'),
+      workflowHeight = this.get('workflowHeight'),
+      offset = this.get('offset');
+
+    var windowHeight = $(window).height();
+    console.log('[duraark-sidebar] windowHeight: ' + windowHeight);
+
+    var sidebarHeight = windowHeight - headerHeight - footerHeight - workflowHeight - offset;
+
+    Ember.run.schedule('afterRender', function() {
+      $el.height(sidebarHeight);
+    });
+  },
+
+  didInsertElement() {
+    this.setFullHeight();
+
+    $(window).resize(function() {
+      this.setFullHeight();
+    }.bind(this));
+  }
+});
