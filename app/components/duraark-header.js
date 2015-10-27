3301cb5d94106cca3fcea4ff64972fedeb9232eb
martinhecher <martin.hecher@fraunhofer.at>
2015-10-27 20:25:16 +0100

    refactored logic into 'duraark-preingest-steps' compmonent

--- app/components/duraark-header.js
+++ app/components/duraark-header.js
@@ -1,25 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  activeStep: 'files',
-
-  filesActive: function() {
-    return this.get('activeStep').toLowerCase() === 'files';
-  }.property('activeStep'),
-
-  metadataActive: function() {
-    return this.get('activeStep').toLowerCase() === 'metadata';
-  }.property('activeStep'),
-
-  semanticenrichmentActive: function() {
-    return this.get('activeStep').toLowerCase() === 'semanticenrichment';
-  }.property('activeStep'),
-
-  geometricenrichmentActive: function() {
-    return this.get('activeStep').toLowerCase() === 'geometricenrichment';
-  }.property('activeStep'),
-
-  digitalpreservationActive: function() {
-    return this.get('activeStep').toLowerCase() === 'digitalpreservation';
-  }.property('activeStep')
 });
