a9b5adb286f28abf7f154a0d8726500ff919a602
martinhecher <martin.hecher@fraunhofer.at>
2015-10-28 22:16:25 +0100

    fixed merge error

--- app/models/building.js
+++ app/models/building.js
@@ -1,12 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-<<<<<<< HEAD
   label: DS.attr('string'),
   latitutde: DS.attr(),
   longitude: DS.attr(),
   url: DS.attr('string')
-=======
-  
->>>>>>> staging
 });
