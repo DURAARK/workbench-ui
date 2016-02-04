c0419fb2b6a6fd100e33ac706f1aa8d308ba31e8
Martin Hecher <martin.hecher@fraunhofer.at>
2016-02-04 20:13:59 +0000

    fixed showall again

--- app/components/x3d-viewer.js
+++ app/components/x3d-viewer.js
@@ -5,38 +5,40 @@ export default Ember.Component.extend({
   inlineUrl: null,
 
   didInsertElement() {
-      // this._init();
-      x3dom.reload();
+    // this._init();
+    x3dom.reload();
+    setTimeout(function() {
       document.getElementById('SCENE').runtime.showAll();
-    },
+    }, 300);
+  },
 
-    inlineUrlChanged: function() {
-      this._render();
-      // x3dom.reload();
-    }.observes('inlineUrl'),
+  inlineUrlChanged: function() {
+    this._render();
+    // x3dom.reload();
+  }.observes('inlineUrl'),
 
-    _init() {
-      this._x3dViewer = new X3DViewer('x3d', this.get('inlineUrl'), function() {
-        // setTimeout(function() {
-          document.getElementById('SCENE').runtime.showAll();
-        // }, 1000);
-      }, 'duraark', 'dummy', '700', false, false, false, false);
-
-    },
-
-    _render() {
-      // this._x3dViewer.deleteScenes();
-      if(!this._x3dViewer) {
-          this._init();
-      }
-
-      this._x3dViewer.inlineScene(this.get('inlineUrl'), 'duraark', function() {
-        document.getElementById('SCENE').runtime.showAll();
-      });
+  _init() {
+    this._x3dViewer = new X3DViewer('x3d', this.get('inlineUrl'), function() {
       // setTimeout(function() {
-      //   let el = document.getElementById('SCENE');
-      //   debugger;
-      //   document.getElementById('SCENE').runtime.showAll();
+      document.getElementById('SCENE').runtime.showAll();
       // }, 1000);
-    },
+    }, 'duraark', 'dummy', '700', false, false, false, false);
+
+  },
+
+  _render() {
+    // this._x3dViewer.deleteScenes();
+    if (!this._x3dViewer) {
+      this._init();
+    }
+
+    this._x3dViewer.inlineScene(this.get('inlineUrl'), 'duraark', function() {
+      document.getElementById('SCENE').runtime.showAll();
+    });
+    // setTimeout(function() {
+    //   let el = document.getElementById('SCENE');
+    //   debugger;
+    //   document.getElementById('SCENE').runtime.showAll();
+    // }, 1000);
+  },
 });
