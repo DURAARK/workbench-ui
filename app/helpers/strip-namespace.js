5f5f060f34a163e394afc7e53422b7099ff25d97
Martin Hecher <martin.hecher@fraunhofer.at>
2016-02-18 15:25:34 +0000

    adapted 'strip-namespace' helper to versioned urls

--- app/helpers/strip-namespace.js
+++ app/helpers/strip-namespace.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
-export function stripNamespace(params/*, hash*/) {
-  return decodeURIComponent(params[0].split('/').pop().replace('_', ' '));
+export function stripNamespace(params /*, hash*/ ) {
+  let url = params[0],
+    hasVersion = false;
+
+  hasVersion = (url.split('/version/').length >= 2);
+
+  if (hasVersion) {
+    url = url.replace(/\/version\/\d*/, '');
+    console.log('url: ' + url);
+  }
+
+  return decodeURIComponent(url.split('/').pop().replace('_', ' '));
 }
 
 export default Ember.HTMLBars.makeBoundHelper(stripNamespace);
